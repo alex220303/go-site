0003a3337dbcf840a4643c30a65590f566286cae
Александр <poni.taw@yandex.ru>
2024-04-28 12:23:30 +0300

    first start with docker

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+     <h1>{{ message }}</h1>
+  </div>
+ </template>
+ 
+ <script>
+ export default {
+  data() {
+     return {
+       message: '',
+     };
+  },
+  mounted() {
+     fetch('http://localhost:9090/ping')
+       .then(response => response.text())
+       .then(data => {
+         this.message = data;
+       });
+  },
+ };
+ </script>
+ 
---
+++ server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+    "github.com/gin-gonic/gin"
+    "github.com/gin-contrib/cors"
+)
+
+func main() {
+    r := gin.Default()
+
+    // Настройка CORS
+    config := cors.DefaultConfig()
+    config.AllowOrigins = []string{"127.0.0.1"} // Разрешаем запросы с любого источника
+    config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
+    config.AllowHeaders = []string{"*"}
+    config.AllowCredentials = true
+
+    r.Use(cors.New(config))
+
+    r.GET("/ping", pong)
+    r.Run(":9090")
+}
+
+func pong(c *gin.Context) {
+    c.JSON(200, gin.H{
+        "message": "pong",
+    })
+}
